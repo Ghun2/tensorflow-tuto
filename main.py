22781aa0dc1b389ace5813f3bee5e0a8d5b2a658
Jihun <ghun2ee@gmail.com>
2021-07-11 20:52:39 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+
+# height = [170, 180, 175, 160]
+# shoes = [260, 270, 265, 255]
+
+height = 170.0
+shoes = 260.0
+
+# 신발 = 키 * a + b
+
+a = tf.Variable(0.1)
+b = tf.Variable(0.2)
+
+
+def loss_function(): # 손실 함수
+    predicted = height * a + b
+    return tf.square(260 - predicted)      # (실제값 - 예측값)^2
+
+
+loss_function()
+
+opt = tf.keras.optimizers.Adam(learning_rate=0.1)
+
+for i in range(300):
+    # opt.minimize(손실함수, var_list=[a, b])
+    opt.minimize(loss_function, var_list=[a, b])
+    print(a.numpy(), b.numpy())
